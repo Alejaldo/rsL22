9636af02c74db3539d9911279dd817a9d4e1e898
Alejaldo <statfluct@yahoo.com>
2020-11-04 02:12:31 +0500

    Add new pages and fetches

--- app.rb
+++ app.rb
@@ -14,3 +14,42 @@ end
 get '/visit' do
 	erb :visit
 end
+
+get '/contacts' do
+	erb :contacts
+end
+
+post '/visit' do
+	@username = params[:username]
+	@phone = params[:phone]
+	@date = params[:date]
+	@time = params[:time]
+	@fcker = params[:fcker]
+
+	@title = 'Good!'
+	@message = "#{@username} (#{@phone}) you have just booked #{@date} at #{@time}. Our #{@fcker} will fck your ass!"
+	
+	guestlist = File.open './public/guestlist.txt', 'a'
+	guestlist.write "Guest #{@username} (phone: #{@phone}) has made appointment on #{@date} at #{@time} with Mr. #{@fcker}\n"
+	guestlist.close
+
+	erb :contacts
+end
+
+get '/admin' do
+	erb :admin
+end 
+
+post '/admin' do 
+	@login = params[:login]
+	@pas = params[:password]
+
+	if @login == 'admin' && @pas == 'obana'
+        @openlist = File.open './public/guestlist.txt', 'r'
+		@zaebisto = '<p>You are axyeNNy chuvak!</p>'
+		erb :admin
+    else
+        @warning = '<p>Wrong login or password!</p>'
+        erb :admin
+    end
+end
